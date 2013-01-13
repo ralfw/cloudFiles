023fb2f6a18bb5a1d1bfe1b10653db84a5c20bc7
Ralf Westphal <info@ralfw.de>
2013-01-13 23:29:30 +0100

    Implemented IKeyValueStore wrapper for IronCache

--- source/cloudfiles/cloudfiles.ironiocache.tests/test_IronIOCache.cs
+++ source/cloudfiles/cloudfiles.ironiocache.tests/test_IronIOCache.cs
@@ -2,10 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using cloudfiles.contract;
 
 namespace cloudfiles.ironiocache.tests
 {
+    [TestFixture]
     public class test_IronIOCache
     {
+        private IronIOCache _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            var cre = IronIOCredentials.LoadFrom(@"..\..\..\..\..\unversioned\ironcache credentials.txt");
+            _sut = new IronIOCache("test", cre);
+        }
+
+
+        [Test]
+        public void Integration()
+        {
+            var value = "";
+
+            _sut.Remove("newkey");
+            _sut.Add("newkey", "hello");
+            Assert.AreEqual("hello", _sut.Get("newkey"));
+            Assert.IsTrue(_sut.TryGet("newkey", out value));
+            Assert.AreEqual("hello", value);
+
+            _sut.Remove("newkey");
+            Assert.Throws<KeyValueStoreException>(() => _sut.Get("newkey"));
+            Assert.IsFalse(_sut.TryGet("newkey", out value));
+
+            _sut.Remove("newkey2");
+            _sut.ReplaceOrAdd("newkey2", "world");
+            Assert.AreEqual("world", _sut.Get("newkey2"));
+            _sut.ReplaceOrAdd("newkey2", "quick brown fox");
+            Assert.AreEqual("quick brown fox", _sut.Get("newkey2"));
+
+            _sut.Remove("newcounter");
+            Assert.AreEqual(1, _sut.Increment("newcounter", 1));
+            Assert.AreEqual(10, _sut.Increment("newcounter", 9));
+        }
     }
 }
--- source/cloudfiles/cloudfiles.ironiocache/IronIOCache.cs
+++ source/cloudfiles/cloudfiles.ironiocache/IronIOCache.cs
@@ -2,45 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web;
 using cloudfiles.contract;
+using io.iron.ironcache;
 
 namespace cloudfiles.ironiocache
 {
     public class IronIOCache : IKeyValueStore
     {
-        public IronIOCache(IronIOCredentials ironIoCredentials)
+        private readonly string _cacheName;
+        private readonly IronCache _cache;
+
+        public IronIOCache(string cacheName, IronIOCredentials ironIoCredentials)
         {
-            
+            _cacheName = cacheName;
+            _cache = new IronCache(ironIoCredentials.ProjectId, ironIoCredentials.Token);
         }
 
         public void Add(string key, string value)
         {
-            throw new NotImplementedException();
+            _cache.Add(_cacheName, key, value);
         }
 
         public void ReplaceOrAdd(string key, string value)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _cache.Replace(_cacheName, key, value);
+            }
+            catch(HttpException)
+            {
+                Add(key, value);
+            }
         }
 
         public int Increment(string key, int amount)
         {
-            throw new NotImplementedException();
+            return _cache.Increment(_cacheName, key, amount);
         }
 
         public string Get(string key)
         {
-            throw new NotImplementedException();
+            var value = _cache.Get<string>(_cacheName, key);
+            if (value != null) return value;
+            throw new KeyValueStoreException(string.Format("No entry found for key {0}", key));
         }
 
         public bool TryGet(string key, out string value)
         {
-            throw new NotImplementedException();
+            value = _cache.Get<string>(_cacheName, key);
+            return value != null;
         }
 
         public void Remove(string key)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _cache.Remove(_cacheName, key);
+            }
+            catch(HttpException) {}
         }
 
         public void Clear()
@@ -48,9 +68,6 @@ namespace cloudfiles.ironiocache
             throw new NotImplementedException();
         }
 
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+        public void Dispose() {}
     }
 }
